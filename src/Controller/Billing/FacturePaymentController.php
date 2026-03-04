6fedd07f24dbcb12bf4d50f0eceb4cc586ce8fbc
Jeroen <jeroen@MacBook-Pro.local>
2026-03-04 01:06:38 +0100

    Update entities + migration

--- src/Controller/Billing/FacturePaymentController.php
+++ src/Controller/Billing/FacturePaymentController.php
@@ -10,9 +10,11 @@ use Symfony\Component\HttpFoundation\{Request, Response};
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+
+
 #[Route('/administrateur/{entite}/facture/{id}/paiement', name: 'app_admin_facture_paiement_')]
 #[IsGranted('ROLE_USER')]
-class FacturePaymentController extends AbstractController
+final class FacturePaymentController extends AbstractController
 {
     #[Route('/checkout', name: 'checkout', methods: ['POST'])]
     public function checkout(Entite $entite, Facture $facture, Request $request, FactureCheckoutManager $manager): Response
